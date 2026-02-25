45044eb4aea0561a757c867530ab0835db20ec11
karthik633 <2400030633@kluniversity.in>
2026-02-25 14:50:23 +0530

    fix base path for vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { AppProvider } from './context/AppContext.jsx'
 
-const Router =
-  window.location.hostname.includes('vercel')
-    ? BrowserRouter
-    : HashRouter
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
+    <BrowserRouter>
       <AppProvider>
         <App />
       </AppProvider>
-    </Router>
+    </BrowserRouter>
   </StrictMode>
 )
