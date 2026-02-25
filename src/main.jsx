f66fa4d1b05a8177babc8f802f03cf5eedcb5dee
karthik633 <2400030633@kluniversity.in>
2026-02-25 14:37:49 +0530

    fix routing for gh pages and vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { AppProvider } from './context/AppContext.jsx'
 
+const Router =
+  window.location.hostname.includes('vercel')
+    ? BrowserRouter
+    : HashRouter
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <Router>
       <AppProvider>
         <App />
       </AppProvider>
-    </HashRouter>
+    </Router>
   </StrictMode>
 )
